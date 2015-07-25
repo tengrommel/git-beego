443af1b25a97b6012feb98911c3ca6b9c67494b7
tengrommel <wwwsousouccom@126.com>
2015-07-26 00:29:14 +0800

    模板

--- beefer.go
+++ beefer.go
@@ -2,14 +2,37 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"text/template"
 )
 
 type BeeferController struct {
 	beego.Controller
 }
 
+type User struct {
+	Username string
+}
+
 func (c *BeeferController) Get() {
-	c.Ctx.WriteString("Hello Beego")
+	//c.Ctx.WriteString("Hello Beego")
+	var tpl string = `
+        <html>
+            <head>
+                <title>Beefer!</title>
+            </head>
+            <body>
+                <strong>Hello, {{.User.Username}}</strong>
+            </body>
+        </html>
+ `
+	data := make(map[interface{}]interface{})
+	user := User{Username: "腾"}
+	data["User"] = user
+
+	t := template.New("Beefer Template")
+	t = template.Must(t.Parse(tpl))
+
+	t.Execute(c.Ctx.ResponseWriter, data)
 }
 
 func main() {
