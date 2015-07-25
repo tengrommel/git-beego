de454a8969141644580b41a9473b8161f65adeaa
tengrommel <wwwsousouccom@126.com>
2015-07-26 01:42:42 +0800

    MVC建立模板

--- beefer.go
+++ beefer.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/astaxie/beego"
-	"text/template"
 )
 
 type BeeferController struct {
@@ -14,25 +13,11 @@ type User struct {
 }
 
 func (c *BeeferController) Get() {
-	//c.Ctx.WriteString("Hello Beego")
-	var tpl string = `
-        <html>
-            <head>
-                <title>Beefer!</title>
-            </head>
-            <body>
-                <strong>Hello, {{.User.Username}}</strong>
-            </body>
-        </html>
- `
-	data := make(map[interface{}]interface{})
+
 	user := User{Username: "腾"}
-	data["User"] = user
+	c.Data["User"] = user
 
-	t := template.New("Beefer Template")
-	t = template.Must(t.Parse(tpl))
-
-	t.Execute(c.Ctx.ResponseWriter, data)
+	c.TplNames = "beefer.tpl"
 }
 
 func main() {
